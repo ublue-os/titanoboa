33aea66c79a0842313d438a1a3ca7450e26ef997
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-03-25 17:27:18 +0100

    fix: Faulty dnf location fetch
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- .github/workflows/ci_dummy_hook_postrootfs.sh
+++ .github/workflows/ci_dummy_hook_postrootfs.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 YAI_LATEST=$(curl -fSsL https://api.github.com/repos/ublue-os/yai/releases/latest | jq -r --arg arch $(arch) '.assets[] | select(.name | match("yai-.*."+$arch+".rpm")) | .browser_download_url')
-{ which dnf5 || which dnf; } 2>/dev/null install -y $YAI_LATEST
+dnf=$(which dnf5 || which dnf)
+$dnf install -y $YAI_LATEST
 mkdir -p /etc/gnome-initial-setup
 tee /etc/gnome-initial-setup/vendor.conf <<EOF
 [live_user pages]
