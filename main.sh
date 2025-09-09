c213119fc7e2682f210a34d1682860e9b664ebf2
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-09-09 15:10:08 +0200

    feat: implement container image unpacking
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

---
+++ main.sh
@@ -0,0 +1,178 @@
+#!/bin/bash
+#
+# Project Titanoboa
+#
+# Description: Create bootable ISOs from bootc container images.
+#
+# shellcheck disable=SC2317
+
+{ return 0 2>/dev/null; } ||
+    : 'Stop interpreting early if we sourced the script'
+
+set -eo pipefail
+
+# Enable verbose debugging
+if [[ ${RUNNER_DEBUG:-0} -eq 1 || ${DEBUG:-0} -eq 1 ]]; then
+    export PS4='+( ${BASH_SOURCE}:${LINENO} ): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+    set -x
+fi
+
+####### region PRIVATE_ENVIROMENTAL_VARS #######
+
+_TITANOBOA_ROOT=$(dirname "$0")
+
+_TITANOBOA_WORKDIR=${_TITANOBOA_ROOT}/work
+
+_TITANOBOA_ISO_ROOTFS=${_TITANOBOA_WORKDIR}/iso-root
+
+# Directory for the root filesystem of the live environment
+_TITANOBOA_ROOTFS=${_TITANOBOA_WORKDIR}/rootfs
+
+_TITANOBOA_CPU_ARCH=$(uname -m)
+
+# Reference to a container image used as an external builder
+_TITANOBOA_BUILDER_IMAGE= # Leave empty to be populated later on based on TITANOBOA_BUILDER_DISTRO
+
+####### endregion PRIVATE_ENVIROMENTAL_VARS #######
+
+#
+#
+#
+
+####### region PUBLIC_ENVIROMENTAL_VARS #######
+
+TITANOBOA_BUILDER_DISTRO=${TITANOBOA_BUILDER_DISTRO:-fedora}
+
+# Hook used for custom operations done in the rootfs before it is squashed.
+TITANOBOA_HOOK_POSTROOTFS=${TITANOBOA_HOOK_POSTROOTFS:-}
+
+# Hook used for custom operations done before the initramfs is generated.
+TITANOBOA_HOOK_PREINITRAMFS=${TITANOBOA_HOOK_PREINITRAMFS:-}
+
+# File with a list of Flatpak applications to install in the rootfs.
+TITANOBOA_FLATPAKS_FILE=${TITANOBOA_FLATPAKS_FILE:-}
+
+####### endregion PUBLIC_ENVIROMENTAL_VARS #######
+
+#
+#
+#
+
+# Set _TITANOBOA_BUILDER_IMAGE based on TITANOBOA_BUILDER_DISTRO
+case ${TITANOBOA_BUILDER_DISTRO} in
+fedora)
+    _TITANOBOA_BUILDER_IMAGE=quay.io/fedora/fedora:latest
+    ;;
+centos)
+    _TITANOBOA_BUILDER_IMAGE=ghcr.io/hanthor/centos-anaconda-builder:main
+    ;;
+almalinux-kitten)
+    _TITANOBOA_BUILDER_IMAGE=quay.io/almalinux/almalinux:10-kitten
+    ;;
+almalinux)
+    _TITANOBOA_BUILDER_IMAGE=quay.io/almalinux/almalinux:10
+    ;;
+*)
+    echo "Unsupported builder distribution: ${TITANOBOA_BUILDER_DISTRO}"
+    exit 1
+    ;;
+esac
+
+#
+#
+#
+
+####### region INNER_FUNCTIONS #######
+
+# Show the configuration used to run Titanoboa. Should be the first thing to show.
+_show_config() {
+    echo "Using the following configuration:"
+    echo "################################################################################"
+    cat <<EOF | column -t
+_TITANOBOA_WORKDIR := ${_TITANOBOA_WORKDIR:?}
+_TITANOBOA_ROOTFS := ${_TITANOBOA_ROOTFS:?}
+_TITANOBOA_CPU_ARCH := ${_TITANOBOA_CPU_ARCH:?}
+_TITANOBOA_BUILDER_IMAGE := ${_TITANOBOA_BUILDER_IMAGE:?}
+_TITANOBOA_BUILDER_DISTRO := ${TITANOBOA_BUILDER_DISTRO:?}
+EOF
+    echo "################################################################################"
+}
+
+# Extract the root filesystem from a container image into _TITANOBOA_ROOTFS
+#
+# Arguments:
+#   $1 - The container image to extract the root filesystem from.
+_unpack_ctr_image_rootfs() {
+    echo >&2 "Executing ${FUNCNAME[0]}..."
+
+    local image=${1:?}
+    local ctr_id
+
+    echo >&2 "Creating container..."
+    echo >&2 "  image=$image"
+    ctr_id=$(sudo podman create --rm "$image" /bin/true)
+    echo >&2 "Container created"
+    echo >&2 "  ctr_id=$ctr_id"
+
+    echo >&2 "Extracting root filesystem..."
+    mkdir -p "$_TITANOBOA_ROOTFS"
+    # shellcheck disable=SC2046
+    sudo podman export "$ctr_id" |
+        sudo env -v -- tar \
+            --extract \
+            --preserve-permissions \
+            --xattrs-include='*' \
+            --file - -C "$_TITANOBOA_ROOTFS"
+    echo >&2 "Root filesystem extracted"
+
+    echo >&2 "Removing leftover container..."
+    sudo podman rm "$ctr_id"
+    echo >&2 "Removed leftover container"
+
+    # Make /var/tmp be a tmpfs by symlinking to /tmp,
+    # in order to make bootc work at runtime.
+    echo >&2 "Symlinking /var/tmp to /tmp..."
+    rm -rf "$_TITANOBOA_ROOTFS/var/tmp"
+    ln -sr "$_TITANOBOA_ROOTFS/tmp" "$_TITANOBOA_ROOTFS/var/tmp"
+    echo >&2 "Symlinked /var/tmp to /tmp"
+
+    echo >&2 "Finished ${FUNCNAME[0]}"
+}
+
+# Execute commands with podman using _TITANOBOA_ROOTFS as the rootfs
+_chroot() {
+    local _CHROOT_ARGS=${_CHROOT_ARGS:-}
+
+    echo >&2 "TODO"
+    return
+    # shellcheck disable=SC2086
+    sudo podman --transient-store run \
+        --rm \
+        -it \
+        --privileged \
+        --security-opt=label=type:unconfined_t \
+        --rootfs ${_TITANOBOA_ROOTFS:?} \
+        --tmpfs=/tmp:rw \
+        --tmpfs=/run:rw \
+        --volume="${_TITANOBOA_ROOT}/pkg":/bin/pkg:ro \
+        ${_CHROOT_ARGS}
+
+}
+
+_chroot_builder() {
+    echo >&2 "TODO"
+}
+
+####### endregion INNER_FUNCTIONS #######
+
+main() {
+    _show_config
+
+    _unpack_ctr_image_rootfs "ghcr.io/ublue-os/bluefin:latest"
+
+    echo >&2 "TODO"
+
+    exit
+}
+
+main "$@"
