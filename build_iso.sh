3a2c97cadd3fb2af40b56a5beb5f866abd801164
Zeglius <33781398+Zeglius@users.noreply.github.com>
2026-02-11 21:51:17 +0100

    chore: set efi size to 100M for now
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- build_iso.sh
+++ build_iso.sh
@@ -74,7 +74,7 @@ cp -avT /work/EFI /work/iso-root/EFI
 
 # Generate uefi.img
 pushd /work || exit 1
-truncate -s $((($(du --apparent-size -sb /work/EFI | cut -f1) * 102 / 100) + 102400)) /work/uefi.img
+truncate -s 100M /work/uefi.img
 mkfs.fat -F32 /work/uefi.img
 mcopy -v -i /work/uefi.img -s /work/EFI ::
 xorriso -as mkisofs \
