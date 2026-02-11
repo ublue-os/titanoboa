7e6d36c2a5d3152abeb5e2a063762bdd7cc7b792
Zeglius <33781398+Zeglius@users.noreply.github.com>
2026-02-11 13:53:52 +0100

    chore: use zstd compression
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- build_iso.sh
+++ build_iso.sh
@@ -14,7 +14,7 @@ mkdir -p \
 cd /work || exit 1
 
 # Create the squashfs image of the container image
-mksquashfs /rootfs /work/iso-root/LiveOS/squashfs.img -all-root -noappend -e sysroot -e ostree
+mksquashfs /rootfs /work/iso-root/LiveOS/squashfs.img -all-root -noappend -e sysroot -e ostree -comp zstd -Xcompression-level 19
 
 iso_config_file=/rootfs/usr/lib/bootc-image-builder/iso.yaml
 if [[ ! -f $iso_config_file ]]; then
